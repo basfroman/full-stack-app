bdb3f0941292de1e228374b89d9e0a22b153c6d4
basfroman <basfroman@gmail.com>
2024-01-18 01:47:42 -0800

    Update BE functionality with MongoDB.

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,46 +1,81 @@
 import express from "express";
-import { cartItems as cartItemsRow, courses as coursesRow} from './temp-data';
-
-let cartItems = cartItemsRow;
-let courses = coursesRow;
-
-const app = express();
-app.use(express.json());
+import { MongoClient } from 'mongodb';
 
 
-app.get('/courses', (req, res) => {
-    res.json(courses);
-})
+const dbUrlPattern = 'mongodb+srv://<user>:<password>@<db_name>.ipb1usb.mongodb.net/?retryWrites=true&w=majority'
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+const dbName = process.env.DB_NAME
+const dbURL = dbUrlPattern.replace('<user>', dbUser).replace('<password>', dbPassword).replace('<db_name>', dbName)
 
-function populateCartIds(ids) {
-    return ids.map(id => courses.find(course => course.id === id));
+const client = new MongoClient(dbURL);
+
+async function startServer() {
+
+    await client.connect();
+    const db = client.db(dbName);
+    const courses = db.collection('courses');
+
+    const app = express();
+    app.use(express.json());
+
+    async function populateCartIds(ids) {
+        return Promise.all(ids.map(id => courses.findOne({ id: id })));
+    }
+
+    async function findCourse(id) {
+        return await courses.findOne({ id: id });
+    }
+    
+    app.get('/courses', async (_, res) => {
+        res.json(await courses.find({}).toArray());
+    });
+    
+    app.get('/courses/:courseId', async (req, res) => {
+        const course = await findCourse(req.params.courseId);
+        res.json(course ? course : {});
+    });
+    
+    app.get('/users/:userId/cart', async (req, res) => {
+        const cartItems = await db.collection('users').findOne({ id: req.params.userId });
+        const populatedCart = await populateCartIds(cartItems ? cartItems.cartItems : []);
+        res.json(populatedCart);
+    });
+    
+    app.post('/users/:userId/cart', async (req, res) => {
+        const userId = req.params.userId;
+        const courseId = req.body.id;
+        
+        if (await findCourse(courseId)) {
+            await db.collection('users').updateOne({'id': userId}, {
+                $addToSet: {cartItems: courseId},
+            });
+
+        } else {
+            res.statusMessage = 'The course to be added was not found.'
+        }
+
+        const cartItems = await db.collection('users').findOne({ id: userId });
+        const populatedCart = await populateCartIds(cartItems ? cartItems.cartItems : []);
+        res.json(populatedCart);
+    });
+    
+    app.delete('/users/:userId/cart/:courseId', async (req, res) => {
+        const userId = req.params.userId;
+        const courseId = req.params.courseId;
+
+        await db.collection('users').updateOne({ id: userId}, {
+            $pull: { cartItems: courseId },
+        });
+
+        const cartItems = await db.collection('users').findOne({ id: userId });
+        const populatedCart = await populateCartIds(cartItems ? cartItems.cartItems : []);
+        res.json(populatedCart);
+    });
+    
+    app.listen(8000, () => {
+        console.log('Server is listening on port 8000')
+    });    
 }
 
-app.get('/cart', (req, res) => {
-    const populatedCart = populateCartIds(cartItems);
-    res.json(populatedCart);
-})
-
-app.post('/cart', (req, res) => {
-    const courseId = req.body.id;
-    cartItems.push(courseId);
-    const populatedCart = populateCartIds(cartItems);
-    res.json(populatedCart);
-})
-
-app.delete('/cart/:courseId', (req, res) => {
-    const courseId = req.params.courseId;
-    cartItems = cartItems.filter(id => id !==courseId)
-    const populatedCart = populateCartIds(cartItems);
-    res.json(populatedCart);
-})
-
-app.get('/courses/:courseId', (req, res) => {
-    const courseId = req.params.courseId;
-    const course = courses.find(course => course.id === courseId);
-    res.json(course);
-})
-
-app.listen(8000, () => {
-    console.log('Server is listening on port 8000')
-})
+startServer();
--- back-end/src/temp-data.js
+++ back-end/src/temp-data.js
@@ -1,33 +1,33 @@
-export const courses = [{
-    id: "01",
-    name: "JavaScript Course",
-    price: '$100.00',
-    // image: jsCourse,
-}, {
-    id: "02",
-    name: "Java Course",
-    price: '$110.00',
-    // image: javaCourse,
-}, {
-    id: "03",
-    name: "Go Course",
-    price: '$90.00',
-    // image: goCourse,
-}, {    
-    id: "04",
-    name: "Python Course",
-    price: '$130.00',
-    // image: pythonCourse,
-}, {
-    id: "05",
-    name: "QA Course",
-    price: '$70.00',
-    // image: qaCourse,
-}, {
-    id: "06",
-    name: "Vue from Scratch",
-    price: '$145',
-    // image: vueCourse,
-}];
+// Data for db.courses
 
-export const cartItems = ['01', '03']
+// {
+//     id: "01",
+//     name: "JavaScript Course",
+//     price: '$100.00',
+//     imageURL: '/image/js.png',
+// }, {
+//     id: "02",
+//     name: "Java Course",
+//     price: '$110.00',
+//     imageURL: '/image/java.png',
+// }, {
+//     id: "03",
+//     name: "Go Course",
+//     price: '$90.00',
+//     imageURL: '/image/go.png',
+// }, {    
+//     id: "04",
+//     name: "Python Course",
+//     price: '$130.00',
+//     imageURL: '/image/py.png',
+// }, {
+//     id: "05",
+//     name: "QA Course",
+//     price: '$70.00',
+//     imageURL: '/image/qa.png',
+// }, {
+//     id: "06",
+//     name: "Vue from Scratch",
+//     price: '$145',
+//     imageURL: '/image/vue.png',
+// }
